6de10f6ae901b908e935676de47b9013f4d6a921
Xabier Portas <105586880+xportas@users.noreply.github.com>
2023-12-19 09:16:30 +0100

    feat(add) add nav top

---
+++ 4v-gym-app/src/app/navbartop/navbartop.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbartop',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbartop.component.html',
+  styleUrl: './navbartop.component.css'
+})
+export class NavbartopComponent {
+
+}

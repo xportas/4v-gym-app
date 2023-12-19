53ad734b2869a92af384d2f49ac33184acf5e2ef
Xabier Portas <105586880+xportas@users.noreply.github.com>
2023-12-19 11:15:54 +0100

    feat(fix and add) add new images and fix services

--- 4v-gym-app/src/app/services/activity.service.ts
+++ 4v-gym-app/src/app/services/activity.service.ts
@@ -1,24 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Instructor } from './instructor.service';
+import { ActivityType } from './activity-type.service';
 
 
 
 
-export enum ActivityType {'BodyPump', 'Pilates', 'Spinning'};
-
-export class Activity {
-  constructor(
-    public activity_date: Date,
-    public instructor: Instructor,
-    public instructor2: Instructor, 
-    public type: ActivityType
-  ) {
-    this.activity_date = activity_date;
-    this.instructor = instructor;
-    this.instructor2 = instructor2;
-    this.type = type;
+  export class Activity {
+    constructor(
+      public activity_date: Date,
+      public instructor: Instructor[],
+      public type: ActivityType
+    ) {
+      this.activity_date = activity_date;
+      this.instructor = instructor;
+      this.type = type;
+    }
   }
-}
 
 
 

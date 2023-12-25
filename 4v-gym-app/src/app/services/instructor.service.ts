f15aed2de242e186723d273d7dbc97ae4b74c0b3
Xabier Portas <105586880+xportas@users.noreply.github.com>
2023-12-25 22:37:49 +0100

    feat(update) update instructors services

--- 4v-gym-app/src/app/services/instructor.service.ts
+++ 4v-gym-app/src/app/services/instructor.service.ts
@@ -22,4 +22,24 @@ export class Instructor {
 export class InstructorService {
 
   constructor() { }
+
+  getInstructors(): Instructor[] {
+    const instructors: Instructor[] = [
+      new Instructor('Xabier Portas', 'xportas@example.com', 123456789),
+      new Instructor('Miguel Goyena', 'miguel@example.com', 987654321),
+      new Instructor('Laura Fernández', 'laura@example.com', 555555555),
+      new Instructor('Aitor Etxeberría', 'aitor@example.com', 111111111),
+      new Instructor('Javier Domínguez', 'javi@example.com', 999999999),
+      new Instructor('Sarah Colleman', 'sarah@example.com', 888888888),
+      new Instructor('Miriam Álvarez', 'miriam@example.com', 777777777),
+      new Instructor('Lúa Castro', 'luacastro@example.com', 666666666),
+      new Instructor('Michael Jordan', 'michael@example.com', 444444444),
+    ];
+
+    return instructors;
+  }
+
+  instructors: Instructor[] = this.getInstructors();
+
+
 }

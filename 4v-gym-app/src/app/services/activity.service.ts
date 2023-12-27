b8ba872365fc5d3dab9e18ef1a844a59477e7e2d
Xabier Portas <105586880+xportas@users.noreply.github.com>
2023-12-27 10:44:09 +0100

    feat(fix) fix activity service

--- 4v-gym-app/src/app/services/activity.service.ts
+++ 4v-gym-app/src/app/services/activity.service.ts
@@ -1,62 +1,56 @@
 import { Injectable } from '@angular/core';
-import { Instructor } from './instructor.service';
-import { InstructorService } from './instructor.service';
+import { InstructorService, Instructor } from './instructor.service';
 
 export enum EnumNames { 'BodyPump', 'Pilates', 'Spinning' };
 
+@Injectable({
+  providedIn: 'root'
+})
 export class Activity {
   
   public image: string;
+  public instructors: Instructor[];
   
   constructor(
     public name: EnumNames,
     public activity_date: Date,
-    public instructor: Instructor[],
+    public instructor1: Instructor,
+    public instructor2?: Instructor,
   ) {
-    this.name = name;
-    if (name == EnumNames.BodyPump){
-      this.image = '../../assets/bodypump.svg'
-    }
-    else if (name == EnumNames.Pilates){
-      this.image = '../../assets/pilates.svg'
-    }
-    else {
-      this.image = '../../assets/spinning.svg'
+    if (name === EnumNames.BodyPump && instructor2) {
+      this.image = '../../assets/bodypump.svg';
+      this.instructors = [instructor1, instructor2];
+    } else if (name === EnumNames.Pilates) {
+      this.image = '../../assets/pilates.svg';
+      this.instructors = [instructor1];
+    } else {
+      this.image = '../../assets/spinning.svg';
+      this.instructors = [instructor1];
     }
     this.activity_date = activity_date;
-    this.instructor = instructor;
   }
 }
-
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActivityService {
 
-  instructorService: any;
+  constructor(private instructorService: InstructorService) { }
 
-
-  constructor() { }
-
-
-  /* generateActivities(): Activity[] {
-    const activities: Activity[] = [
-      new Activity(EnumNames.BodyPump, new Date('7 Jan 2024 11:00:00 +0000'), [this.instructorService.instructors[0], this.instructorService.instructors[1]]),
-      new Activity(EnumNames.Pilates, new Date('7 Jan 2024 18:00:00 +0000'), [this.instructorService.instructors[2], this.instructorService.instructors[3]]),
-      new Activity(EnumNames.BodyPump, new Date('8 Jan 2024 14:00:00 +0000'), [this.instructorService.instructors[5], this.instructorService.instructors[4]]),
-      new Activity(EnumNames.Spinning, new Date('9 Jan 2024 11:00:00 +0000'), [this.instructorService.instructors[0], this.instructorService.instructors[1]]),
-      new Activity(EnumNames.Spinning, new Date('9 Jan 2024 14:00:00 +0000'), [this.instructorService.instructors[1], this.instructorService.instructors[2]]),
+  generateActivities(): Activity[] {
+    return [
+      new Activity(EnumNames.BodyPump, new Date('7 Jan 2024 11:00:00 +0000'), this.instructorService.instructors[0], this.instructorService.instructors[1]),
+      new Activity(EnumNames.Pilates, new Date('7 Jan 2024 18:00:00 +0000'), this.instructorService.instructors[2]),
+      new Activity(EnumNames.BodyPump, new Date('8 Jan 2024 14:00:00 +0000'), this.instructorService.instructors[5], this.instructorService.instructors[4]),
+      new Activity(EnumNames.Spinning, new Date('9 Jan 2024 11:00:00 +0000'), this.instructorService.instructors[3]),
+      new Activity(EnumNames.Spinning, new Date('9 Jan 2024 14:00:00 +0000'), this.instructorService.instructors[1]),
     ];
-    return this.activities;
-  } */
+  }
 
+  activities: Activity[] = this.generateActivities();
 
-  //activities: Activity[] = this.generateActivities();
-
-  // addActivity(act:Activity) {
-  //   this.activities.push(act);
-  // }
-
+  addActivity(act: Activity) {
+    this.activities.push(act);
+  }
 }

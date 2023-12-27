3f96fa6e08c7c98946efb20215fbe831ed04a1d6
Xabier Portas <105586880+xportas@users.noreply.github.com>
2023-12-27 12:00:39 +0100

    feat(update) update datebox

--- 4v-gym-app/src/app/datepicker/datepicker.component.ts
+++ 4v-gym-app/src/app/datepicker/datepicker.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatCardModule} from '@angular/material/card';
-import {MatNativeDateModule} from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule } from '@angular/material/core';
 import { DailyboardComponent } from '../dailyboard/dailyboard.component';
 
 @Component({
@@ -12,19 +12,28 @@ import { DailyboardComponent } from '../dailyboard/dailyboard.component';
   styleUrl: './datepicker.component.css'
 })
 export class DatepickerComponent {
+  monthsOfTheYear = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+  
   selectedDay: Date | null = new Date(Date.now());
+  dateToShow: string = this.selectedDay?`${this.selectedDay.getDate()}  ${this.monthsOfTheYear[this.selectedDay.getMonth()]}  ${this.selectedDay.getFullYear()}`:'';
 
-  previousDay(){
+  previousDay() {
     if (this.selectedDay) {
       this.selectedDay = new Date(this.selectedDay);
       this.selectedDay.setDate(this.selectedDay.getDate() - 1);
+      
+      // Build the date to show to user
+      this.dateToShow = `${this.selectedDay.getDate()}  ${this.monthsOfTheYear[this.selectedDay.getMonth()]}  ${this.selectedDay.getFullYear()}`;
+      // --> special quotes to isert the variable value
+      //month go from 0 to 11, if i want to get december = 12, i should +1
     }
   }
 
-  nextDay(){
+  nextDay() {
     if (this.selectedDay) {
       this.selectedDay = new Date(this.selectedDay);
       this.selectedDay.setDate(this.selectedDay.getDate() + 1);
+      this.dateToShow = `${this.selectedDay.getDate()}  ${this.monthsOfTheYear[this.selectedDay.getMonth()]}  ${this.selectedDay.getFullYear()}`;
     }
   }
 

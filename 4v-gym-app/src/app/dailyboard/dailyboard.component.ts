b60a1510695113875f99035900217ab37ecb1eca
Xabier Portas <105586880+xportas@users.noreply.github.com>
2023-12-27 11:15:34 +0100

    feat(fix) fix daily board interface

--- 4v-gym-app/src/app/dailyboard/dailyboard.component.ts
+++ 4v-gym-app/src/app/dailyboard/dailyboard.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { ActivityboardComponent } from '../activityboard/activityboard.component';
 import { ModalactivityComponent } from '../modalactivity/modalactivity.component';
 
 @Component({
   selector: 'app-dailyboard',
   standalone: true,
-  imports: [ActivityboardComponent, ModalactivityComponent],
+  imports: [ModalactivityComponent],
   templateUrl: './dailyboard.component.html',
   styleUrl: './dailyboard.component.css'
 })

ab44e46ec2228abded7a0f5bdccb14115048adb0
Xabier Portas <105586880+xportas@users.noreply.github.com>
2023-12-27 13:14:37 +0100

    feat(update) update dailyboard methods

--- 4v-gym-app/src/app/dailyboard/dailyboard.component.ts
+++ 4v-gym-app/src/app/dailyboard/dailyboard.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChanges } from '@angular/core';
 import { ModalactivityComponent } from '../modalactivity/modalactivity.component';
+import { Input } from '@angular/core';
+import { InstructorService, Instructor } from '../services/instructor.service';
+import { ActivityService, Activity } from '../services/activity.service';
 
 @Component({
   selector: 'app-dailyboard',
@@ -9,5 +12,33 @@ import { ModalactivityComponent } from '../modalactivity/modalactivity.component
   styleUrl: './dailyboard.component.css'
 })
 export class DailyboardComponent {
+
+  // Initializes services
+  constructor(public activityService: ActivityService, public instructorService: InstructorService) { }
+
+  // Getting its father´s data
+  @Input() selectedDay: any;
+
+  // Activities in this day
+  currentActivities: Activity[] = [];
+
+
+  // Function that is listening changes into selectedDate and add activities to currentActivities
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['selectedDay']) {
+      let newSelectedDay = changes['selectedDay'].currentValue;
+      let aux = this.activityService.activities;
+      for (let i = 0; i < aux.length; i++){
+        if (aux[i].activity_date.getFullYear() == newSelectedDay.getFullYear() &&
+          aux[i].activity_date.getMonth() == newSelectedDay.getMonth() && 
+          aux[i].activity_date.getDay() == newSelectedDay.getDay()){
+            this.currentActivities.push(aux[i]);
+        }
+        if (this.currentActivities.length >= 3){
+          return;
+        }
+      }
+    }
+  }
 
 }

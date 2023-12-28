8bdf58cc8b2a2f83b6b9a504c9e10187d4c9320d
Xabier Portas <105586880+xportas@users.noreply.github.com>
2023-12-28 16:09:39 +0100

    feat(update) update activity modal

--- 4v-gym-app/src/app/modalactivity/modalactivity.component.ts
+++ 4v-gym-app/src/app/modalactivity/modalactivity.component.ts
@@ -1,26 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // IMPORT THIS BECAUSE IF YOU DONT IMPORT IT NGFOR CANT WORK
 import { ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
 import { ActivitiesService, Activity } from '../services/activities.service';
-import { NgIf } from '@angular/common';
+import { InstructorService, Instructor } from '../services/instructor.service';
+import { NgIf, NgFor } from '@angular/common';
 
 
 @Component({
   selector: 'app-modalactivity',
   standalone: true,
-  imports: [ReactiveFormsModule, NgIf],
+  imports: [ReactiveFormsModule, NgIf, CommonModule], // Import CommonModule for correct ngFor work
   templateUrl: './modalactivity.component.html',
   styleUrl: './modalactivity.component.css'
 })
 export class ModalactivityComponent {
 
-  constructor(public activitiesService: ActivitiesService) { }
+  constructor(public activitiesService: ActivitiesService, public instructorService: InstructorService) { }
 
   allActivities: Activity[] = this.activitiesService.activities;
+  allInstructors: Instructor[] = this.instructorService.instructors;
+
+  instrAssistant: boolean = false;
 
   actType = new FormControl('', [Validators.required]);
   instOne = new FormControl('', [Validators.required]);
   instTwo = new FormControl('');
 
-
+  onActTypeChange() {
+    // if actType === BodyPump user can choose anotherone assistant instructor
+    this.instrAssistant = this.actType.value === 'BodyPump';
+  }
 
 }
